aa11354d8009c88b90ac77240865601853a1e8cc
Keith Doelling <kd889@nyu.edu>
2018-01-05 15:08:09 -0500

    update parameter of the model to be closer to oscillator state.

--- simosc.m
+++ simosc.m
@@ -41,7 +41,7 @@ function [ brainOut, E, I ] = simosc( soundIn, SR, coupling, tau )
     
     % Solve the phase_coupling set of equations.Options fixs the accepted value of error
     options = odeset('RelTol',1e-6,'AbsTol',[1e-6 1e-6 1e-6 1e-6 1e-6 1e-6]);
-    Yant = [ .5 0.3];
+    Yant = [ 0 0];
     
     %%% Solve the diff equation in steps in order to not to get the cumulative
     %%% errors.
@@ -56,7 +56,7 @@ function [ brainOut, E, I ] = simosc( soundIn, SR, coupling, tau )
         % continuous variable
         tspan = (nStep-1)*delta_t:1/Fs:nStep*delta_t;
         [Ttemp, Ytemp] = ode45(@wilsonCowan, tspan,...
-            [Yant(1) Yant(2) auditory(nStep) 1.5 -3.2 tau], options); % Solve ODE (2.3 -3.2)
+            [Yant(1) Yant(2) auditory(nStep) 2.3 -3.2 tau], options); % Solve ODE (2.3 -3.2)
         Yant(1)=Ytemp(end,1);
         Yant(2)=Ytemp(end,2);
         Y = [Y; Ytemp];
